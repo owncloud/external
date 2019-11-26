6368f2a7c511ed854ccf282ad54f8fd826533041
Dominik Schmidt <domme@tomahawk-player.org>
2019-11-26 15:03:56 +0000

    [tx] updated from transifex

--- l10n/lv.js
+++ l10n/lv.js
@@ -1,9 +1,15 @@
 OC.L10N.register(
     "external",
     {
-    "External Sites" : "Ārējās saites",
+    "Please enter valid urls - they have to start with either http://, https:// or /" : "Lūdzu, ievadiet derīgus URL - viņiem ir jāsākas ar http://, https:// vai /",
+    "External sites saved." : "Ārējās vietnes saglabātas.",
+    "External Sites" : "Ārējās vietnes",
+    "Please note that some browsers will block displaying of sites via http if you are running https." : "Lūdzu, ņemiet vērā, ka dažas pārlūkprogrammas var bloķēt vietņu attēlošanu, kuras izmantojot http, ja jūs izmantojat https.",
+    "Furthermore please note that many sites these days disallow iframing due to security reasons." : "Lūdzu, ņemiet vērā, ka daudzās vietnēs mūsdienās drošības apsvērumu dēļ nav atļauts izmantot \"iframe\".",
+    "We highly recommend to test the configured sites below properly." : "Mēs ļoti iesakām pārbaudīt zemāk esošās konfigurētās vietnes.",
     "Name" : "Nosaukums",
     "URL" : "URL",
+    "Select an icon" : "Atlasiet ikonu",
     "Remove site" : "Izņemt vietni",
     "Add" : "Pievienot"
 },
