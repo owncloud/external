06a172e079b31ed46d0a93f2ca527d395c55247a
ownClouders <devops@owncloud.com>
2022-12-02 01:37:26 +0000

    [tx] updated from transifex

--- l10n/bg_BG.js
+++ l10n/bg_BG.js
@@ -1,12 +1,12 @@
 OC.L10N.register(
     "external",
     {
-    "Please enter valid urls - they have to start with either http://, https:// or /" : "Моля, въведи валидни интернет адреси - те трябва да започват с http://, https:// или /",
+    "Please enter valid urls - they have to start with either http://, https:// or /" : "Моля, въведете валидни адреси - те трябва да започват с http://, https:// или /",
     "External sites saved." : "Външните сайтове са запазени.",
     "External Sites" : "Външни сайтове",
-    "Please note that some browsers will block displaying of sites via http if you are running https." : "Да знаеш, че някои браузъри ще блокират показването на интернет страници през http ако същевременно използваш https.",
-    "Furthermore please note that many sites these days disallow iframing due to security reasons." : "И още, моля, да знаеш, че много интернет страници в наши дни не разрешават използването на iframe от съображения за сигурност.",
-    "We highly recommend to test the configured sites below properly." : "Силно препоръчваме да тестваш настроените сайтове.",
+    "Please note that some browsers will block displaying of sites via http if you are running https." : "Моля, имайте предвид, че някои браузъри ще блокират показването на сайтове чрез http, ако използвате https.",
+    "Furthermore please note that many sites these days disallow iframing due to security reasons." : "Освен това имайте предвид, че в днешно време много сайтове от съображения за сигурност не допускат iframing.",
+    "We highly recommend to test the configured sites below properly." : "Силно препоръчваме да тествате правилно конфигурираните сайтове по-долу.",
     "Name" : "Име",
     "URL" : "URL адрес",
     "Select an icon" : "Изберете икона",
